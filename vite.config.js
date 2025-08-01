10280b72ee00dc6090504bee65440eddeb2de9df
nina <nina.e@systemli.org>
2025-08-01 18:12:43 +0200

    Add dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { config } from 'dotenv';
+
+config();
 
 // https://vite.dev/config/
 export default defineConfig({
