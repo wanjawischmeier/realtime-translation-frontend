f9f6454d0acf85bf6d01a04855b20a73c890a361
substrato <movementwiki@systemli.org>
2025-08-08 10:54:38 +0200

    Add basic stats

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import WhisperLiveKitStreamer from "./views/WhisperLiveKitStreamer";
 import WhisperLiveKitViewer from "./views/WhisperLiveKitViewer";
 import { ServerHealthProvider } from "./components/ServerHealthContext";
 import WebSocketViewer from "./views/WebSocketViewer";
-import {AuthProvider} from "./components/AuthContext";
+import { AuthProvider } from "./components/AuthContext";
 import HeaderHandler from "./components/HeaderHandler";
 import RouteProtect from "./components/RouteProtect"
 import ToastProvider from "./components/ToastProvider"
@@ -13,25 +13,25 @@ import ToastProvider from "./components/ToastProvider"
 export default function App() {
   return (
     <AuthProvider>
-    <ToastProvider>
-    <ServerHealthProvider wsUrl={import.meta.env.VITE_BACKEND_URL}>
-      <Router>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
-          <div className="relative bg-gray-800 rounded-none shadow-lg sm:min-w-150 p-2 sm:p-4 pt-15 w-full min-h-screen sm:min-h-100 sm:w-auto sm:rounded-xl">
-            <HeaderHandler></HeaderHandler>
-            <Routes>
-              <Route path="/" element={<StartView />} />
-              <Route path="/ws_debug" element={<WebSocketViewer wsUrl={import.meta.env.VITE_BACKEND_URL} />} />
-              <Route path="/rooms" element={<RoomListView wsUrl={import.meta.env.VITE_BACKEND_URL} />} />
-              <Route path="/rooms/host" element={<RouteProtect element={<RoomListView wsUrl={import.meta.env.VITE_BACKEND_URL} asHost={true} />} />} />
-              <Route path="/room/:room_id/host" element={<RouteProtect element={<WhisperLiveKitStreamer />} />} />
-              <Route path="/room/:room_id/view" element={<WhisperLiveKitViewer />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </ServerHealthProvider>
-    </ToastProvider>
+      <ToastProvider>
+        <ServerHealthProvider wsUrl={import.meta.env.VITE_BACKEND_URL}>
+          <Router>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+              <div className="relative bg-gray-800 rounded-none shadow-lg sm:min-w-150 p-2 sm:p-4 pt-15 w-full min-h-screen sm:min-h-100 sm:w-auto sm:rounded-xl">
+                <HeaderHandler></HeaderHandler>
+                <Routes>
+                  <Route path="/" element={<StartView />} />
+                  <Route path="/ws_debug" element={<WebSocketViewer wsUrl={import.meta.env.VITE_BACKEND_URL} />} />
+                  <Route path="/rooms" element={<RoomListView wsUrl={import.meta.env.VITE_BACKEND_URL} />} />
+                  <Route path="/rooms/host" element={<RouteProtect element={<RoomListView wsUrl={import.meta.env.VITE_BACKEND_URL} asHost={true} />} />} />
+                  <Route path="/room/:room_id/host" element={<RouteProtect element={<WhisperLiveKitStreamer />} />} />
+                  <Route path="/room/:room_id/view" element={<WhisperLiveKitViewer />} />
+                </Routes>
+              </div>
+            </div>
+          </Router>
+        </ServerHealthProvider>
+      </ToastProvider>
     </AuthProvider>
   );
 }
