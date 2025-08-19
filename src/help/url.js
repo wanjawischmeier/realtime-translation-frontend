ba7e1a0c09225ef9de9e7a0ca0dfc2f51ea29b3f
nina <nina.e@systemli.org>
2025-08-19 10:12:56 +0200

    Add missing reference

--- src/help/url.js
+++ src/help/url.js
@@ -1,6 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export function getBackendUrl() {
+    const location = useLocation();
+
     return (location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_URL : import.meta.env.VITE_BACKEND_SECURE_URL
 }
 export function getBackendWs() {
+    const location = useLocation();
+
     return (location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_WS : import.meta.env.VITE_BACKEND_SECURE_WS
 }
