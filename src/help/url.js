0c4f5233edcff1dd13da03e76c60a3ba216fd964
nina <nina.e@systemli.org>
2025-08-19 10:50:44 +0200

    Fixup url.js

--- src/help/url.js
+++ src/help/url.js
@@ -1,12 +1,6 @@
-import { useLocation } from "react-router-dom";
-
 export function getBackendUrl() {
-    const location = useLocation();
-
-    return (location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_URL : import.meta.env.VITE_BACKEND_SECURE_URL
+    return (window.location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_URL : import.meta.env.VITE_BACKEND_SECURE_URL
 }
 export function getBackendWs() {
-    const location = useLocation();
-
-    return (location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_WS : import.meta.env.VITE_BACKEND_SECURE_WS
+    return (window.location.protocol !== 'https:') ? import.meta.env.VITE_BACKEND_WS : import.meta.env.VITE_BACKEND_SECURE_WS
 }
